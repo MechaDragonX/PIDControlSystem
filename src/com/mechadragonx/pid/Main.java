3f87c89fe5d08316d97e69c2ba871837428588f6
MechaDragonX <theofficialrv@gmail.com>
2020-05-01 15:54:31 -0700

    Remove uneccesary code

--- src/com/mechadragonx/pid/Main.java
+++ src/com/mechadragonx/pid/Main.java
@@ -3,15 +3,12 @@ package com.mechadragonx.pid;
 public class Main {
     public static void main(String[] args) {
         PIDController controller = new PIDController(0.15, 0.1, 0.1, 60, 1, 4);
-
         double current = 40;
         int iteration = 0;
-        double output = 0;
 
         while(iteration < 100) {
             iteration++;
-            output =  controller.getOutput(current);
-            current += output;
+            current += controller.getOutput(current);
             System.out.println(iteration + ": " + current);
         }
     }
